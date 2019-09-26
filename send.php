9dbfbfbb1e1c628ae5cc080b6e8cb7489038e63b
Anton Mandrichenko <arnellomutty2@gmail.com>
2019-09-26 09:57:00 +0300

    Created tables users

--- send.php
+++ send.php
@@ -1,33 +1,70 @@
 <?php
-$fio = $_POST['fio'];
+$name = $_POST['name'];
 $email = $_POST['email'];
-$fio = htmlspecialchars($fio);
+$name = htmlspecialchars($name);
 $email = htmlspecialchars($email);
-$fio = urldecode($fio);
+$name = urldecode($name);
 $email = urldecode($email);
-$fio = trim($fio);
+$name = trim($name);
 $email = trim($email);
 
-$subject = "Confirm your e-mail";
-$message = "<html>
-<body>
-    <h1>Confirm your e-mail</h1>
-    <p>Click for confirm <a href='http://www.chromeextention.space/confirm.php'>here</a> </p>
-</body>
-</html>" ;
-$message = wordwrap($message, 70, "\r\n");
-$headers = "From: www//.chromeextention.space \r\n";
+$token   = md5($email);
+$to      = $email; // Send email to our user
+$subject = 'Confirm your e-mail'; // Give the email a subject 
+$headers = "From: wwww//.chromeextention.space \r\n"; // Set from headers
 $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-$token = md5($email);
+$message = '
+ 
+Thanks your order \r\n
+ 
+------------------------\r\n
+Name: '.$name.'\r\n
+Email: '.$email.'\r\n
+------------------------\r\n
+ 
+Please click this link to activate your account:\r\n
+http://www.chromeextention.space/confirm.php?'.$token.'
+ 
+'; // Our message above including the link
 
-if (mail($email, $subject , $message, $headers)) {
-    echo "<h2>You have received a confirmation email to your email</h2>";
-    echo $token;
-} else {
-    echo "при отправке сообщения возникли ошибки";
+mail($to, $subject, $message, $headers); // Send our email
+
+
+$servername = 'mg363964.mysql.tools';
+$username = 'mg363964_db';
+$password = 'wst8AKQt';
+$dbname = 'mg363964_db';
+
+$conn = new mysqli($servername, $username, $password, $dbname); // Create connection
+if ($conn->connect_errno) {     // Check connection
+    die("Connection failed: " . $conn->connect_error . "\nservername: " . $servername . "\ndbname:" . $dbname . "\nusername:" . $username . "\npassword: " . $password);
+} 
+
+$users = "CREATE TABLE IF NOT EXISTS `users` (
+    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `username` varchar(255) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `token` varchar(255) NOT NULL
+    )";
+
+if ($conn->query($users) === TRUE) {
+    $username_text = mysqli_real_escape_string($conn, $name);
+    $email_text = mysqli_real_escape_string($conn, $email);
+    $token_text = mysqli_real_escape_string($conn, $token);
 }
+
+$sql = "INSERT INTO users (date, username, email, token)
+VALUES (CURDATE(),  '$username_text', '$email_text', '$token_text') ON DUPLICATE KEY UPDATE    
+date=CURDATE(),  username='$username_text', email='$email_text', token='$token_text'";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Page saved!";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
 ?>
 
-<script>
-    localStorage.setItem('cashspotusa', '<?php echo $token;?>');  
-</script>
