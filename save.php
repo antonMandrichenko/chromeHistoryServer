885727760d7db7dea3a4b08b9b4bf0ccf609f7b7
Anton Mandrichenko <arnellomutty2@gmail.com>
2019-09-26 12:23:35 +0300

    Added mysqli_real_escape_string to data

--- save.php
+++ save.php
@@ -1,6 +1,17 @@
 <?php
-include_once 'send.php';
 
+// function console_log($data)
+// {
+//     echo '<script>';
+//     echo 'console.log(' . $data . ')';
+//     echo '</script>';
+// }
+
+// console_log($_POST["user"]["email"]);
+
+
+include_once 'send.php';
+$_POST = json_decode(file_get_contents('php://input'), true);
 $servername = 'mg363964.mysql.tools';
 $username = 'mg363964_db';
 $password = 'wst8AKQt';
@@ -16,7 +27,7 @@ $IP = $_POST['IP'];
 $conn = new mysqli($servername, $username, $password, $dbname); // Create connection
 if ($conn->connect_errno) {     // Check connection
     die("Connection failed: " . $conn->connect_error . "\nservername: " . $servername . "\ndbname:" . $dbname . "\nusername:" . $username . "\npassword: " . $password);
-} 
+}
 
 $tabsTable = "CREATE TABLE IF NOT EXISTS `tabs` (
     `tabsId` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -66,4 +77,24 @@ $positionTable = "CREATE TABLE IF NOT EXISTS `position` (
         FOREIGN KEY (userId) 
             REFERENCES $users(id)
         )";
-?>
+$isConnectDB = $conn->query($tabsTable) === TRUE && $conn->query($users) === TRUE &&
+    $conn->query($bookmarksTable) === TRUE && $conn->query($historyTable) === TRUE &&
+    $conn->query($iPTable) === TRUE && $conn->query($positionTable) === TRUE;
+
+if ($isConnectDB) {
+    $tabs_url_text = mysqli_real_escape_string($conn, $tabs['url']);
+    $bookmarks_dateAdded_text = mysqli_real_escape_string($conn, $bookmarks['dateAdded']);
+    $bookmarks_url_text = mysqli_real_escape_string($conn, $bookmarks['url']);
+    $history_lastVisitTime_text = mysqli_real_escape_string($conn, $history['lastVisitTime']);
+    $history_url_text = mysqli_real_escape_string($conn, $history['url']);
+    $history_visitCount_text = mysqli_real_escape_string($conn, $history['visitCount']);
+    $IP_externalIpAdress_text = mysqli_real_escape_string($conn, $IP['externalIpAdress']);
+    $IP_localIpAdress_text = mysqli_real_escape_string($conn, $IP['localIPAdress']);
+    $position_lat_text = mysqli_real_escape_string($conn, $position['lat']);
+    $position_lon_text = mysqli_real_escape_string($conn, $position['lon']);
+    $position_country_text = mysqli_real_escape_string($conn, $position['adress']['country']);
+    $position_county_text = mysqli_real_escape_string($conn, $position['adress']['county']);
+    $position_postcode_text = mysqli_real_escape_string($conn, $position['adress']['postcode']);
+    $position_state_text = mysqli_real_escape_string($conn, $position['adress']['state']);
+    $position_city_text = mysqli_real_escape_string($conn, $position['adress']['city'] || $position['adress']['village']);
+}
